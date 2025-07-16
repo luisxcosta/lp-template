d9b1ea4484893e78c8d789d9dea1b8e233727c94
luisxcosta <luiscosta@Luiss-MacBook-Pro-4.local>
2025-07-16 14:21:58 -0400

    blank landing page

--- my-landing/src/app/page.tsx
+++ my-landing/src/app/page.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Page = () => {
     return (
         <div>
-            <h1>Hello, World!</h1>
+            <h1>Blank Landing Page Created!</h1>
         </div>
     );
 };
